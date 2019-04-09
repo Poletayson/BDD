6c41359ddc1b56527e0765079e8818aff7287d23
Poletayson <dmitriipoletaev_1997@mail.ru>
2019-04-09 21:36:07 +0700

    Модифицирован метод открытия файла. Все тесты проходят, метод готов

--- steps/Program.py
+++ steps/Program.py
@@ -14,6 +14,7 @@ class Example:
         if (str == "jpg" or str == "png" or str == "jpeg"):
             self.img = Image.open(self.filename)
             return str
-
+        else:
+            return '...'
 
 
